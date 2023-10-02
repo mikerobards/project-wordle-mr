8b7f1dd792c2335a176b8a7b3f1051ef8e37bb1d
Michael Robards <mikerobards@icloud.com>
2023-10-02 17:27:14 -0400

    add grid and fill guess

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import { range } from '../../utils';
+
+import Guess from '../Guess/Guess';
+
 function GuessResults({ guesses }) {
   return (
-    <ul className="guess-results">
-      {guesses.map((guess, index) => (<li className="guess" key={index}>{guess}</li>))}
-    </ul>
+    <div className="guess-results">
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess
+          key={num}
+          value={guesses[num]} />
+      ))}
+    </div>
   );
 }
 
