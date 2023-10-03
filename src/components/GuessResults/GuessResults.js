196f03454bb62d45d34b201fd44d2875f70bc425
Michael Robards <mikerobards@icloud.com>
2023-10-03 15:53:40 -0400

    add guess logic

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -5,13 +5,14 @@ import { range } from '../../utils';
 
 import Guess from '../Guess/Guess';
 
-function GuessResults({ guesses }) {
+function GuessResults({ guesses, answer }) {
   return (
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
         <Guess
           key={num}
-          value={guesses[num]} />
+          value={guesses[num]}
+          answer={answer} />
       ))}
     </div>
   );
